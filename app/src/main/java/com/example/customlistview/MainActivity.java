3e3207bba9ee4520f8a2f3ef4a58b9e50dd3651e
rahmatsob <rahmatdij31@gmail.com>
2020-11-30 14:40:39 +0700

    MovieDirectori

---
+++ app/src/main/java/com/example/customlistview/MainActivity.java
@@ -0,0 +1,156 @@
+package com.example.customlistview;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+
+    String mTitle[] = {"Joker", "Malefience: Mistress Of Evil", "Terminator: Dark Fate", "El Camino: A breaking Bad Movie", "The Angry Birds Movie 2"};
+    String mDescription[] = {"During the 1980, a failed stand-up comedian is driven insane and turn to...", "Maleficent and her goddaughter Aurora begin to question the complex family ti...", "More than two decades have passed since Sarah Connor prevented judgme...", "In the wake of his dramatic escape from captivity, Jesse Pinkman Must come to...", "Red, Chuck, Bomb and the rest of their feathered friends are surprised when a green pig suggests that they put aside..."};
+    String mTayang[] = {"2019-10-04","2019-10-18","2019-11-01","2019-10-11","2019-08-14"};
+    int images[] = {R.drawable.jkr, R.drawable.ml, R.drawable.trsn, R.drawable.camino, R.drawable.agbrd};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.listView);
+
+        MyAdapter adapter = new MyAdapter(this, mTitle, mDescription, mTayang, images);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                if (position == 0) {
+                    Intent intent = new Intent(getApplicationContext(), AnotherActivity.class);
+                    // this intent put our 0 index image to another activity
+                    Bundle bundle = new Bundle();
+                    bundle.putInt("image", images[0]);
+                    intent.putExtras(bundle);
+                    // now put title and description to another activity
+                    intent.putExtra("title", mTitle[0]);
+                    intent.putExtra("description", mDescription[0]);
+                    intent.putExtra("tayang", mTayang[0]);
+                    // also put your position
+                    intent.putExtra("position", ""+0);
+                    startActivity(intent);
+
+
+                }
+                if (position == 1) {
+                    Intent intent = new Intent(getApplicationContext(), AnotherActivity.class);
+                    // this intent put our 0 index image to another activity
+                    Bundle bundle = new Bundle();
+                    bundle.putInt("image", images[1]);
+                    intent.putExtras(bundle);
+                    // now put title and description to another activity
+                    intent.putExtra("title", mTitle[1]);
+                    intent.putExtra("description", mDescription[1]);
+                    intent.putExtra("tayang", mTayang[1]);
+                    // also put your position
+                    intent.putExtra("position", ""+1);
+                    startActivity(intent);
+                }
+                if (position == 2) {
+                    Intent intent = new Intent(getApplicationContext(), AnotherActivity.class);
+                    // this intent put our 0 index image to another activity
+                    Bundle bundle = new Bundle();
+                    bundle.putInt("image", images[2]);
+                    intent.putExtras(bundle);
+                    // now put title and description to another activity
+                    intent.putExtra("title", mTitle[2]);
+                    intent.putExtra("description", mDescription[2]);
+                    intent.putExtra("tayang", mTayang[2]);
+                    // also put your position
+                    intent.putExtra("position", ""+2);
+                    startActivity(intent);
+                }
+                if (position == 3) {
+                    Intent intent = new Intent(getApplicationContext(), AnotherActivity.class);
+                    // this intent put our 0 index image to another activity
+                    Bundle bundle = new Bundle();
+                    bundle.putInt("image", images[3]);
+                    intent.putExtras(bundle);
+                    // now put title and description to another activity
+                    intent.putExtra("title", mTitle[3]);
+                    intent.putExtra("description", mDescription[3]);
+                    intent.putExtra("tayang", mTayang[3]);
+                    // also put your position
+                    intent.putExtra("position", ""+3);
+                    startActivity(intent);
+                }
+                if (position == 4) {
+                    Intent intent = new Intent(getApplicationContext(), AnotherActivity.class);
+                    // this intent put our 0 index image to another activity
+                    Bundle bundle = new Bundle();
+                    bundle.putInt("image", images[4]);
+                    intent.putExtras(bundle);
+                    // now put title and description to another activity
+                    intent.putExtra("title", mTitle[4]);
+                    intent.putExtra("description", mDescription[4]);
+                    intent.putExtra("tayang", mTayang[4]);
+                    // also put your position
+                    intent.putExtra("position", ""+4);
+                    startActivity(intent);
+                }
+            }
+        });
+    }
+
+    class MyAdapter extends ArrayAdapter<String> {
+
+        Context context;
+        String rTitle[];
+        String rDescription[];
+        String rTayang[];
+        int rImages[];
+
+        MyAdapter (Context c, String title[], String description[], String tayang[], int imgs[]) {
+            super(c, R.layout.row, R.id.textView1, title);
+            this.context = c;
+            this.rTitle = title;
+            this.rDescription = description;
+            this.rTayang = tayang;
+            this.rImages = imgs;
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            LayoutInflater layoutInflater = (LayoutInflater)getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+            View row = layoutInflater.inflate(R.layout.row, parent, false);
+
+            ImageView images = row.findViewById(R.id.image);
+            TextView myTitle = row.findViewById(R.id.textView1);
+            TextView myDescription = row.findViewById(R.id.textView2);
+            TextView myTayang = row.findViewById(R.id.textView3);
+
+            images.setImageResource(rImages[position]);
+            myTitle.setText(rTitle[position]);
+            myDescription.setText(rDescription[position]);
+            myTayang.setText(rTayang[position]);
+
+            return row;
+        }
+    }
+}
